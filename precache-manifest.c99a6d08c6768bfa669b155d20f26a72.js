3fbe0c1081803cf6a79bb3266e0667208c552e3c
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2019-02-04 22:46:30 -0200

    Updates

---
+++ precache-manifest.c99a6d08c6768bfa669b155d20f26a72.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9620da67d5c5a639257d",
+    "url": "/ipfs-share/static/js/main.9620da67.chunk.js"
+  },
+  {
+    "revision": "4aed9ba75a7a996f9cb0",
+    "url": "/ipfs-share/static/js/1.4aed9ba7.chunk.js"
+  },
+  {
+    "revision": "ca95adc6453031ff127a",
+    "url": "/ipfs-share/static/js/runtime~main.ca95adc6.js"
+  },
+  {
+    "revision": "32f6e0e88a0677f569864d5add7eef50",
+    "url": "/ipfs-share/index.html"
+  }
+];
